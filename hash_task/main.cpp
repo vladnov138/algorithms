0de47317b068bf24ff22db867031c608e091a0e5
vladnov138 <vladnov138@gmail.com>
2023-05-13 13:44:35 +0800

    Hash by cpp added, map added, etc

--- hash_task/main.cpp
+++ hash_task/main.cpp
@@ -5,62 +5,79 @@
 #include <fstream>
 #include <chrono>
 #include <vector>
-
+#include <sstream>
+#include <iomanip>
 #include <map>
-#include <conio.h>
+#include <algorithm>
 
-int R(char ki);
-int hash_by_crc(std::string str);
-int hash_by_pjw(std::string str);
-int hash_by_buz(std::string str);
-int find_duplicates(int(*hashfunc)(std::string));
+int hash_by_crc(const std::string &str);
+int hash_by_pjw(const std::string &str);
+int hash_by_buz(const std::string &str);
+int hash_by_cpp(const std::string &str);
+int find_duplicates(const std::vector<std::string>& filenames, int(*hashfunc)(const std::string&));
+void get_filenames(const std::string& path, std::vector<std::string> &filenames);
+void fill_map();
 
-int(*hashes[])(std::string) = { hash_by_crc, hash_by_pjw, hash_by_buz };
+
+int(*hashes[])(const std::string&) = { hash_by_crc, hash_by_pjw, hash_by_buz, hash_by_cpp };
+static std::map<char, int> r;
 
 int main() {
-	for (int i = 0; i < sizeof(hashes) / sizeof(hashes[0]); i++) {
+	const std::string func_names[4] = {"Hash by CRC", "Hash by PJW", "Hash by BUZ", "Hash by CPP"};
+	const std::string path = "out";
+	
+	std::vector<std::string> filenames;
+	get_filenames(path, filenames);
+	fill_map();
+
+	for (int i = 0; i < (int) (sizeof(hashes) / sizeof(hashes[0])) && 
+			i < (int) (sizeof(func_names) / sizeof(func_names[0])); i++) {
 		auto start = std::chrono::high_resolution_clock::now();
-		find_duplicates(hashes[i]);
+		int counter_files = find_duplicates(filenames, hashes[i]);
 		auto finish = std::chrono::high_resolution_clock::now();
 		std::chrono::duration time = finish - start;
-		std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(time).count() << " ms" << std::endl;
+		std::cout << func_names[i] << " " << std::setw(5) <<
+			std::chrono::duration_cast<std::chrono::milliseconds>(time).count() << " ms" << 
+			"\tRepeat files: " << counter_files << std::endl;
 	}
 	return 1;
 }
 
-int find_duplicates(int(*hashfunc)(std::string)) {
+void fill_map() {
+	srand(time(NULL));
+	for (int i = 'A'; i < 'z'; i++)
+		r[i] = rand();
+}
+
+void get_filenames(const std::string& path, std::vector<std::string>& filenames) {
+	for (const auto& entry : std::filesystem::directory_iterator(path))
+		if (entry.is_regular_file())
+			filenames.push_back(entry.path().string());
+}
+
+int find_duplicates(const std::vector<std::string>& filenames, int(*hashfunc)(const std::string&)) {
 	std::string path = "out";
 	std::vector<int> hashes;
 	int counter_repeats = 0;
-	for (const auto& entry : std::filesystem::directory_iterator(path))
-	{
-		if (entry.is_regular_file()) {
-			std::ifstream file(entry.path());
-			if (!file.is_open())
-				return -1;
-			while (!file.fail()) {
-				std::string word;
-				file >> word;
-				int h = hashfunc(word);
-				if (std::find(hashes.begin(), hashes.end(), h) == hashes.end())
-					counter_repeats++;
-				else
-					hashes.push_back(h);
-			}
-			file.close();
-		}
+	for (auto filename : filenames) {
+		std::ifstream file(filename);
+		if (!file.is_open())
+			return -1;
+		std::stringstream buf;
+		buf << file.rdbuf();
+		int h = hashfunc(buf.str());
+		if (std::find(hashes.begin(), hashes.end(), h) != hashes.end())
+			counter_repeats++;
+		else
+			hashes.push_back(h);
+		file.close();
 	}
 	return counter_repeats;
 }
 
-int R(char ki) {
-	srand(time(NULL));
-	return rand() * (int) ki;
-}
-
-int hash_by_crc(std::string str) {
+int hash_by_crc(const std::string &str) {
 	int h = 0;
-	for (int i = 0; i < str.length(); i++) {
+	for (int i = 0; i < (int) str.length(); i++) {
 		int highorder = h & 0xf8000000;
 		h = h << 5;
 		h = h ^ (highorder >> 27);
@@ -69,9 +86,9 @@ int hash_by_crc(std::string str) {
 	return h;
 }
 
-int hash_by_pjw(std::string str) {
+int hash_by_pjw(const std::string &str) {
 	int h = 0;
-	for (int i = 0; i < str.length(); i++) {
+	for (int i = 0; i < (int) str.length(); i++) {
 		h = (h << 4) + str[i];
 		int g = h & 0xf0000000;
 		if (g != 0) {
@@ -82,13 +99,17 @@ int hash_by_pjw(std::string str) {
 	return h;
 }
 
-int hash_by_buz(std::string str) {
+int hash_by_buz(const std::string &str) {
 	int h = 0;
-	for (int i = 0; i < str.length(); i++) {
+	for (int i = 0; i < (int) str.length(); i++) {
 		int highorder = h & 0x80000000;
 		h = h << 1;
 		h = h ^ (highorder >> 31);
-		h = h ^ R(str[i]);
+		h = h ^ r[str[i]];
 	}
 	return h;
+}
+
+int hash_by_cpp(const std::string &str) {
+	return std::hash<std::string>{}(str);
 }
